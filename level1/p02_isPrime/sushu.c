e7630ac76fb46387290923aed0c636d09348865e
Pluto-wei <2634002591@qq.com>
2020-03-08 02:45:55 +0800

    pushBoxes1.1

--- level1/p02_isPrime/sushu.c
+++ level1/p02_isPrime/sushu.c
@@ -4,7 +4,7 @@
 
 bool isPrime (int n){
     bool flag = true;
-    if(n==1) return false;
+    if(1==n) return false;   //1==n原来也可以
     int sqr = (int)sqrt((double)n);
     for(int i=2;i<=sqr;i++){
         if(n%i==0) flag=false;
--- level1/p07_encrypt_decrypt/encrypt_decrypt.c
+++ level1/p07_encrypt_decrypt/encrypt_decrypt.c
@@ -1,40 +1,51 @@
 //先写了一遍，然后git add之后忘记git commit直接push了，后来不知道干了啥本地代码就不见了。。
 //然后就重写了一遍。。自闭了呜呜呜，还好这个比较简单
+
+//结果重写完后又找回来了orz
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <malloc.h>
 
-void encrypt(char *str,char *dark){
+void encrypt(char *str,char *dark){ //加密
     int i;
     for(i=0;*(str+i)!='\0';i++){
         if(*(str+i)>='a'&&*(str+i)<='z'){
             *(dark+i)='a'+'z'-*(str+i);
         }else if(*(str+i)>='A'&&*(str+i)<='Z'){
             *(dark+i)='A'+'Z'-*(str+i);
-        }else *(dark+i)=*(str+i)+3;
-    }
+        }else *(dark+i)=*(str+i)-1;
+    }//printf("%s\n",dark);
 }
 
-void decrypt(char *str,char *clear){
+void decrypt(char *str,char* clear){   //解密
     int i;
-    for(i=0;*(str+i)!='\0';i++){
+    for(int i=0;*(str+i)!='\0';i++){
         if(*(str+i)>='a'&&*(str+i)<='z'){
             *(clear+i)='a'+'z'-*(str+i);
         }else if(*(str+i)>='A'&&*(str+i)<='Z'){
             *(clear+i)='A'+'Z'-*(str+i);
-        }else *(clear+i)=*(str+i)-3;
+        }else *(clear+i)=*(str+i)+1;
     }
+    //printf("%s\n",clear);
 }
 
-int main(){
-    char *str=(char*)malloc(sizeof(char)),*clear=(char*)malloc(sizeof(char)),*dark=(char*)malloc(sizeof(char));
+int main (){
+    char *str=(char*)malloc(sizeof(char)),*dark=(char*)malloc(sizeof(char)),*clear=(char*)malloc(sizeof(char));
+
     gets(str);
+    //scanf("%s",str);
+
     encrypt(str,dark);
     puts(dark);
+    //printf("%s\n",dark);
+
     decrypt(dark,clear);
     puts(clear);
+    //printf("%s\n",clear);
+
     if(!strcmp(str,clear)){
         printf("true\n");
-    }else printf("false\n");
-    return 0;
+    }else printf("false\n");    
+
 }
